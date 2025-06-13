8abb958e470ef418b1147730f6b32c2b587d8878
yousuf-safin <yousufalisafin@gmail.com>
2025-06-13 23:26:55 +0600

    first commit

--- classroom.sql
+++ classroom.sql
@@ -1,1 +1,13 @@
- 
+create database college; 
+use college;
+
+create table student (
+	id int primary key,
+    name varchar(50),
+    age int not null
+);
+
+insert into student values (1,"Yousuf",25);
+insert into student values (2,"Yousha",22);
+
+select * from student;
